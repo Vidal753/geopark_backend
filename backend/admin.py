b9cff1f8f8b234a848ad356b1e8ddc86963eb61b
Vidal Francisco  Baquedano <devbooster@Vidals-MacBook-Air.local>
2023-06-18 10:02:57 -0600

    set-up-base-models

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import City, User, News
+
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            "General",
+            {"fields": ["username", "first_name", "last_name", "email", "city"]},
+        ),
+        (
+            "Privacidad",
+            {"fields": ["is_staff", "is_active", "password"]},
+        ),
+    ]
+
+    list_display = ("username", "first_name", "last_name", "email", "city")
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "date", "city")
+    list_filter = ["city"]
--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(verbose_name="Nombre", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Ciudad"
+        verbose_name_plural = "Ciudades"
+
+
+class User(AbstractUser):
+    city = models.OneToOneField(
+        City, verbose_name="Cuidad", on_delete=models.CASCADE, null=True, blank=True
+    )
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+
+class News(models.Model):
+    banner = models.ImageField(null=True, blank=True, upload_to="images/")
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name="Cuidad")
+    title = models.CharField(verbose_name="Titulo", max_length=50)
+    date = models.DateField(verbose_name="Fecha")
+    description = models.TextField(verbose_name="Descripción", max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Noticia"
+        verbose_name_plural = "Noticias"
